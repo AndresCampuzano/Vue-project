0506057c70616627c83ebd20449fb54db19087ca
Andres Campuzano <campuxano88@gmail.com>
2020-05-26 17:48:21 -0500

    Two-way binding.

---
+++ app.js
@@ -0,0 +1,47 @@
+new Vue({
+	el: '#app',
+
+	data() {
+		return {
+			name: 'Bitcoin',
+			symbol: 'BTC',
+			img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+			changePercent: 10,
+			color: '4f4f4f',
+			price: 8400,
+			value: 0,
+			pricesWithDays: [
+				{ day: 'Lunes', value: 8400 },
+				{ day: 'Martes', value: 7900 },
+				{ day: 'Miercoles', value: 8200 },
+				{ day: 'Jueves', value: 9000 },
+				{ day: 'Viernes', value: 9400 },
+				{ day: 'Sabado', value: 10000 },
+				{ day: 'Domingo', value: 10200 },
+			],
+
+			showPrices: false,
+		};
+	},
+
+	computed: {
+		title() {
+			return `${this.name} - ${this.symbol}`;
+		},
+
+		convertedValue() {},
+	},
+
+	watch: {
+		showPrices(newVal, oldVal) {
+			console.log(newVal, oldVal);
+		},
+	},
+
+	methods: {
+		toggleShowPrices() {
+			this.showPrices = !this.showPrices;
+			this.color = this.color.split('').reverse().join('');
+		},
+	},
+});
